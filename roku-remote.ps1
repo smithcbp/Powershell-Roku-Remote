da440948b2185b440c700a3eff9b85fdd3aff65e
smithcbp <38805678+smithcbp@users.noreply.github.com>
2018-04-28 20:04:20 -0800

    Initial commit

---
+++ roku-remote.ps1
@@ -0,0 +1,234 @@
+﻿<#
+.NAME
+    Roku-Remote
+.SYNOPSIS
+    Powershell and WPF based Roku Remote that automatically finds Rokus on the local network.
+.SYNTAX
+    Just run the script to launch the GUI
+.DESCRIPTRION
+   Powershell and WPF based Roku Remote that automatically finds Rokus on the local network.
+.REMARKS
+    Thank you to POSHGUI.com for help with the GUI
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName PresentationFramework
+
+#region begin GUI{
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = '400,600'
+$Form.text                       = "Roku Remote"
+$Form.TopMost                    = $false
+$Form.BackColor                  = "DarkBlue"
+
+$UpButton                        = New-Object system.Windows.Forms.Button
+$UpButton.text                   = "▲"
+$UpButton.width                  = 60
+$UpButton.height                 = 60
+$UpButton.location               = New-Object System.Drawing.Point(171,256)
+$UpButton.Font                   = 'Microsoft Sans Serif,16'
+$UpButton.BackColor              = "Cyan"
+
+$DownButton                      = New-Object system.Windows.Forms.Button
+$DownButton.text                 = "▼"
+$DownButton.width                = 60
+$DownButton.height               = 60
+$DownButton.location             = New-Object System.Drawing.Point(171,387)
+$DownButton.Font                 = 'Microsoft Sans Serif,16'
+$DownButton.BackColor            = "Cyan"
+
+$RightButton                     = New-Object system.Windows.Forms.Button
+$RightButton.text                = "►"
+$RightButton.width               = 60
+$RightButton.height              = 60
+$RightButton.location            = New-Object System.Drawing.Point(237,321)
+$RightButton.Font                = 'Microsoft Sans Serif,30'
+$RightButton.TextAlign           = "MiddleCenter"
+$RightButton.BackColor           = "Cyan"
+
+$SelectButton                    = New-Object system.Windows.Forms.Button
+$SelectButton.text               = "OK"
+$SelectButton.width              = 60
+$SelectButton.height             = 60
+$SelectButton.location           = New-Object System.Drawing.Point(171,321)
+$SelectButton.Font               = 'Microsoft Sans Serif,12'
+$SelectButton.BackColor          = "Cyan"
+
+$LeftButton                      = New-Object system.Windows.Forms.Button
+$LeftButton.text                 = "◄"
+$LeftButton.width                = 60
+$LeftButton.height               = 60
+$LeftButton.location             = New-Object System.Drawing.Point(105,321)
+$LeftButton.BackColor            = "Cyan"
+$LeftButton.Font                 = 'Microsoft Sans Serif,30'
+
+
+$BackButton                      = New-Object system.Windows.Forms.Button
+$BackButton.text                 = "Back"
+$BackButton.width                = 87
+$BackButton.height               = 50
+$BackButton.location             = New-Object System.Drawing.Point(78,195)
+$BackButton.Font                 = 'Microsoft Sans Serif,12'
+$BackButton.BackColor            = "Cyan"
+
+$HomeButton                      = New-Object system.Windows.Forms.Button
+$HomeButton.text                 = "Home"
+$HomeButton.width                = 87
+$HomeButton.height               = 50
+$HomeButton.location             = New-Object System.Drawing.Point(236,195)
+$HomeButton.Font                 = 'Microsoft Sans Serif,12'
+$HomeButton.BackColor            = "Cyan"
+
+$InfoButton                      = New-Object system.Windows.Forms.Button
+$InfoButton.text                 = "*"
+$InfoButton.width                = 50
+$InfoButton.height               = 50
+$InfoButton.location             = New-Object System.Drawing.Point(176,520)
+$InfoButton.Font                 = 'Microsoft Sans Serif,24'
+$InfoButton.TextAlign            = "MiddleCenter"
+$InfoButton.BackColor            = "Cyan"
+
+$RRButton                        = New-Object system.Windows.Forms.Button
+$RRButton.text                   = "«"
+$RRButton.width                  = 50
+$RRButton.height                 = 50
+$RRButton.location               = New-Object System.Drawing.Point(110,460)
+$RRButton.Font                   = 'Microsoft Sans Serif,16'
+$RRButton.BackColor              = "Cyan"
+
+$PlayButton                      = New-Object system.Windows.Forms.Button
+$PlayButton.text                 = "►"
+$PlayButton.width                = 50
+$PlayButton.height               = 50
+$PlayButton.location             = New-Object System.Drawing.Point(176,460)
+$PlayButton.Font                 = 'Microsoft Sans Serif,16'
+$PlayButton.BackColor            = "Cyan"
+
+$FFButton                        = New-Object system.Windows.Forms.Button
+$FFButton.text                   = "»"
+$FFButton.width                  = 50
+$FFButton.height                 = 50
+$FFButton.location               = New-Object System.Drawing.Point(240,460)
+$FFButton.Font                   = 'Microsoft Sans Serif,16'
+$FFButton.BackColor              = "Cyan"
+
+$RebootButton                    = New-Object system.Windows.Forms.Button
+$RebootButton.text               = "Reboot"
+$RebootButton.width              = 100
+$RebootButton.height             = 50
+$RebootButton.location           = New-Object System.Drawing.Point(260,520)
+$RebootButton.Font               = 'Microsoft Sans Serif,12'
+$RebootButton.BackColor          = "Cyan"
+
+$AppsButton                      = New-Object system.Windows.Forms.Button
+$AppsButton.text                 = "Apps"
+$AppsButton.width                = 100
+$AppsButton.height               = 50
+$AppsButton.location             = New-Object System.Drawing.Point(40,520)
+$AppsButton.Font                 = 'Microsoft Sans Serif,12'
+$AppsButton.BackColor            = "Cyan"
+
+$RokuList                        = New-Object system.Windows.Forms.ListBox
+$RokuList.width                  = 360
+$RokuList.height                 = 130
+$RokuList.location               = New-Object System.Drawing.Point(20,54)
+$RokuList.Font                   = 'Consolas,14'
+$RokuList.HorizontalScrollbar    = $true
+
+$Label1                          = New-Object system.Windows.Forms.Label
+$Label1.text                     = "Select your Roku"
+$Label1.AutoSize                 = $true
+$Label1.width                    = 25
+$Label1.height                   = 10
+$Label1.location                 = New-Object System.Drawing.Point(62,10)
+$Label1.Font                     = 'Consolas,18'
+$Label1.ForeColor                = "Cyan"
+
+$Form.controls.AddRange(@($UpButton,$DownButton,$RightButton,$SelectButton,$LeftButton,$BackButton,$HomeButton,$RebootButton,$AppsButton,$RokuList,$Label1,$InfoButton,$RRButton,$PlayButton,$FFButton))
+
+#region gui events
+
+$UpButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-up -ip $selectedroku.ip
+    })
+
+$DownButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-down -ip $selectedroku.ip
+    })
+
+$RightButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-right -ip $selectedroku.ip
+    })
+
+$selectButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-select -ip $selectedroku.ip
+    })
+
+$LeftButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-left -ip $selectedroku.ip
+    })
+
+$BackButton.Add_Click({    
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-back -ip $selectedroku.ip
+    })
+
+$HomeButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-home -ip $selectedroku.ip
+    })
+
+$InfoButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-info -ip $selectedroku.ip
+    })
+
+$RRButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-rev -ip $selectedroku.ip
+    })
+
+$PlayButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-play -ip $selectedroku.ip
+    })
+
+$FFButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    press-select -ip $selectedroku.ip
+    })
+
+$AppsButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    launch-rokuapp -ip $selectedroku.ip
+    })
+
+$RebootButton.Add_Click({
+    $selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+    RebootMacro -ip $selectedroku.ip
+    })
+
+#endregion
+
+#endregion GUI
+
+Import-Module (Resolve-Path('roku-remote.psm1'))
+
+$rokus = Discover-Rokus
+$rokus | ForEach-Object {[void] $RokuList.Items.Add($_.Description)}
+$selectedroku = $rokus | Where-Object Description -Like $RokuList.SelectedItem
+$rokuurl = "http://" + $selectedroku.ip + ":8060"
+
+$result = $Form.ShowDialog()
+
+
+
+
---
+++ roku-remote.psm1
@@ -0,0 +1,254 @@
+﻿<#
+.SYNOPSIS
+
+A collection of functions for discovering and controlling local Roku devices.
+Import-Module .\roku-commands.psm1
+
+.EXAMPLE
+
+PS C:\Scripts> Discover-Rokus
+Ip            Name                          Model                Description
+--            ----                          -----                -----------
+192.168.0.110 Bedroom Roku Stick           Roku Stick 3500X     Bedroom Roku Stick | 192.168.0.110
+192.168.0.111 Roku Express                 Roku Express 3700X   Roku Express  | 192.168.0.111
+
+PS C:\Scripts> Press-home 192.168.0.110
+
+StatusCode        : 200
+StatusDescription : OK
+Content           : {}
+RawContent        : HTTP/1.1 200 OK
+                    Content-Length: 0
+                    Server: Roku UPnP/1.0 MiniUPnPd/1.4
+
+
+Headers           : {[Content-Length, 0], [Server, Roku UPnP/1.0 MiniUPnPd/1.4]}
+RawContentLength  : 0
+
+
+#>
+
+Function Discover-Rokus {
+
+$rokuouis = "DC-3A-5E","D0-4D-2C","CC-6D-A0","C8-3A-6B","B8-A1-75","B8-3E-59","B0-EE-7B","B0-A7-37","AC-3A-7A","88-DE-A9","08-05-81","00-0D-4B"
+$rokuips = @()
+$rokus = @()
+
+ForEach($oui in $rokuouis) {
+    $rokuips += Get-NetNeighbor -LinkLayerAddress $oui*
+    }
+
+$rokus = ForEach ($rokuip in $rokuips) {
+    $ip = $rokuip.IPAddress | Out-String
+    $ip = $ip.Trim()
+    $uri = "http://" + $ip + ":8060"
+    $rokuweb = (Invoke-WebRequest -UseBasicParsing $uri)
+    [xml]$rokuxml = $rokuweb.Content
+    $rokuname = $rokuxml.root.device.friendlyName
+    $rokumodel = $rokuxml.root.device.modelname + " " + $rokuxml.root.device.modelnumber
+    [pscustomobject]@{
+        Ip = $ip
+        Name = $rokuname
+        Model = $rokumodel
+        Description = $rokuname + " | " + $ip
+        }
+    }
+Write-Output $rokus
+}
+
+Function Press-Home {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Home" -method Post 
+    }
+
+Function Press-Rev {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Rev" -method Post 
+    }
+
+Function Press-Fwd {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Fwd" -method Post 
+    }
+
+Function Press-Play{
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Play" -method Post 
+    }
+
+Function Press-Select {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Select" -method Post 
+    }
+
+Function Press-Left {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Left" -method Post 
+    }
+
+Function Press-Right {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Right" -method Post 
+    }
+
+Function Press-Down {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Down" -method Post 
+    }
+
+Function Press-Up {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Up" -method Post 
+    }
+
+Function Press-Back {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Back" -method Post 
+    }
+
+Function Press-InstantReplay {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/InstantReplay" -method Post 
+    }
+
+Function Press-Info {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Info" -method Post 
+    }
+
+Function Press-Backspace {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Backspace" -method Post 
+    }
+
+Function Press-Search {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Search" -method Post 
+    }
+
+Function Press-Enter {
+    param([string]$ip)   
+    $rokuurl = "http://" + $ip + ":8060"
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/keypress/Enter" -method Post 
+    }
+
+Function RebootMacro {
+    param([string]$ip)
+    Press-Home $ip
+    sleep -s 2
+    Press-Home $ip
+    sleep -s 2
+    Press-Down $ip
+    sleep -s 1
+    Press-Down $ip
+    sleep -s 1
+    Press-Down $ip
+    sleep -s 1
+    Press-Down $ip
+    sleep -s 1
+    Press-Down $ip
+    sleep -s 1
+    Press-Down $ip
+    sleep -s 1
+    Press-Down $ip
+    sleep -s 1
+    Press-Select $ip
+    sleep -s 2
+    Press-Up $ip
+    sleep -s 1
+    Press-Right $ip
+    sleep -s 1 
+    Press-Up $ip
+    sleep -s 1 
+    Press-Up $ip
+    sleep -s 1
+    Press-Up $ip
+    sleep -s 1
+    Press-Right $ip
+    sleep -s 1
+    Press-Select $ip 
+    }
+
+Function Launch-RokuApp {
+    param([string]$ip)
+    
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+    Add-Type -AssemblyName System.Drawing
+    Add-Type -AssemblyName PresentationFramework
+    
+    #region Apps Listbox Gui
+    $form                           = New-Object System.Windows.Forms.Form
+    $form.Text                      = 'Select an App'
+    $form.Size                      = New-Object System.Drawing.Size(350,640)
+    $form.StartPosition             = 'CenterScreen'
+    $form.BackColor                 = "DarkBlue"
+
+    $OKButton                       = New-Object System.Windows.Forms.Button
+    $OKButton.Location              = New-Object System.Drawing.Point(75,545)
+    $OKButton.Size                  = New-Object System.Drawing.Size(70,40)
+    $OKButton.Text                  = 'OK'
+    $OKButton.DialogResult          = [System.Windows.Forms.DialogResult]::OK
+    $OKButton.Font                  = 'Microsoft Sans Serif,12'
+    $OKButton.BackColor             = "Cyan"
+    $form.AcceptButton              = $OKButton
+
+    $CancelButton                   = New-Object System.Windows.Forms.Button
+    $CancelButton.Location          = New-Object System.Drawing.Point(190,545)
+    $CancelButton.Size              = New-Object System.Drawing.Size(70,40)
+    $CancelButton.Text              = 'Cancel'
+    $CancelButton.DialogResult      = [System.Windows.Forms.DialogResult]::Cancel
+    $CancelButton.Font              = 'Microsoft Sans Serif,10'
+    $CancelButton.BackColor         = "Cyan"
+    $form.CancelButton              = $CancelButton
+
+    $label                          = New-Object System.Windows.Forms.Label
+    $label.Location                 = New-Object System.Drawing.Point(10,15)
+    $label.Size                     = New-Object System.Drawing.Size(280,20)
+    $label.Text                     = 'PLEASE SELECT AN APP:'
+    $label.Font                     = 'Consolas,12'
+    $label.ForeColor                = "Cyan"
+
+    $form.Controls.AddRange(@($label,$CancelButton,$OKButton))
+
+    $listBox                        = New-Object System.Windows.Forms.ListBox
+    $listBox.Location               = New-Object System.Drawing.Point(10,40)
+    $listBox.Size                   = New-Object System.Drawing.Size(313,20)
+    $listBox.Height                 = 500
+    $listBox.Font                   = New-Object System.Drawing.Font("Comic Sans MS",16,[System.Drawing.FontStyle]::Bold)
+
+    #endregion
+
+    #Select App
+    $rokuurl = "http://" + $ip + ":8060"
+    $appsweb = Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/query/apps" -method Get
+    [xml]$appsxml = $appsweb.Content
+    $apps = $appsxml.apps.app | select "#text",id -ExpandProperty "#text" | sort "#text"
+    foreach ($app in $apps){
+        [void] $listBox.Items.Add($app)
+    }
+
+    $form.Controls.Add($listBox)
+    $form.Topmost = $true
+    $form.ShowDialog()
+
+    $selectedapp = $listBox.SelectedItem
+    $app = $apps | Where-Object "#text" -Like $selectedapp
+    $appid = $app.id | Out-String
+    Invoke-WebRequest -UseBasicParsing -Uri "$rokuurl/launch/$appid" -method Post
+
+}
